f368c906587400ba0672f0a914b534bea9490d16
MateoID <m.isazad@uniandes.edu.co>
2025-01-29 19:38:32 -0500

    Update model3.R

--- model3.R
+++ model3.R
@@ -1,6 +1,6 @@
-# Fill here the code to estimate the linear_model3 from the notebook
+db_int<-db_int %>% mutate(m1_std_residuals = studres(linear_model))# Fill here the code to estimate the linear_model3 from the notebook
 
-db_int <- db_int %>% filter(m1_std_residuals<2 & m1_std_residuals>-2 )
+db_int <- db_int %>% filter(m1_std_residuals < 2 & m1_std_residuals > -2)
 
 linear_model3<- lm(
   totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender
