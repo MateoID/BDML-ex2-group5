479a12175153480238d059cf5e334a81886af85c
haroldacuna <haroldstivacu@gmail.com>
2025-01-29 19:24:52 -0500

    Update model3.R

--- model3.R
+++ model3.R
@@ -1,1 +1,5 @@
 # Fill here the code to estimate the linear_model3 from the notebook
+linear_model3<- lm(
+  totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender
+                                 + H_Head + age + gender,
+  data=db_int)
