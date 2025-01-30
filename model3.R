f3e291f5dbca9efc4bf9581b154edae496f0ab7b
haroldacuna <haroldstivacu@gmail.com>
2025-01-29 19:29:38 -0500

    Update model3.R

--- model3.R
+++ model3.R
@@ -1,4 +1,7 @@
 # Fill here the code to estimate the linear_model3 from the notebook
+
+db_int <- db_int %>% filter(m1_std_residuals<2 & m1_std_residuals>-2 )
+
 linear_model3<- lm(
   totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender
                                  + H_Head + age + gender,
